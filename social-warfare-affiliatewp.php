f38119bae9ddf2902fff4911bb64bc78d78ca6a3
Nicholas Cardot <nick@sitesketch101.com>
2017-09-12 10:06:48 -0500

    Added min core version checks
    These checks will ensure that the user is running a version of core that
    contains the necessary functions for the addon to work. This is set via
    a constant at the top. None of the plugins functions will do anything if
    the minimum core version is not met.

--- social-warfare-affiliatewp.php
+++ social-warfare-affiliatewp.php
@@ -21,21 +21,30 @@ define( 'SWAWP_PLUGIN_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
 define( 'SWAWP_PLUGIN_DIR', dirname( __FILE__ ) );
 define( 'SWAWP_STORE_URL', 'https://warfareplugins.com' );
 define( 'SWAWP_ITEM_ID', 114264 );
+define( 'SWAWP_CORE_VERSION_REQUIRED' , '2.3.2');
 
 /**
  * Add a registration key for the registration functions
  *
  * @param Array An array of registrations for each paid addon
  * @return Array An array modified to add this new registration key
+ *
  */
 add_filter('swp_registrations' , 'social_warfare_affiliatewp_registration_key' , 10);
 function social_warfare_affiliatewp_registration_key($array) {
-    $array['affiliatewp'] = array(
-        'plugin_name' => 'Social Warfare - AffiliateWP',
-        'key' => 'affiliatewp',
-        'product_id' => SWAWP_ITEM_ID
-    );
 
+    // Make sure core is on a version that contains our dependancies
+    if (defined('SWP_VERSION') && version_compare(SWP_VERSION , SWAWP_CORE_VERSION_REQUIRED) >= 0){
+
+        // Add this plugin to the registrations array
+        $array['affiliatewp'] = array(
+            'plugin_name' => 'Social Warfare - AffiliateWP',
+            'key' => 'affiliatewp',
+            'product_id' => SWAWP_ITEM_ID
+        );
+    }
+
+    // Return the modified or unmodified array
     return $array;
 }
 
@@ -50,23 +59,27 @@ function social_warfare_affiliatewp_registration_key($array) {
 add_action( 'plugins_loaded' , 'swawp_update_checker' , 20 );
 function swawp_update_checker() {
 
-    if( is_swp_addon_registered( 'affiliatewp' ) ) {
+    // Make sure core is on a version that contains our dependancies
+    if (defined('SWP_VERSION') && version_compare(SWP_VERSION , SWAWP_CORE_VERSION_REQUIRED) >= 0){
 
+        // Check if the plugin is registered
+        if( is_swp_addon_registered( 'affiliatewp' ) ) {
 
-        // retrieve our license key from the DB
-        $license_key = swp_get_license_key('affiliatewp');
-        $website_url = swp_get_site_url();
+            // retrieve our license key from the DB
+            $license_key = swp_get_license_key('affiliatewp');
+            $website_url = swp_get_site_url();
 
-        // setup the updater
-        $edd_updater = new SW_EDD_SL_Plugin_Updater( SWAWP_STORE_URL , __FILE__ , array(
-        	'version'   => SWAWP_VERSION,		// current version number
-        	'license'   => $license_key,	// license key
-        	'item_id'   => SWAWP_ITEM_ID,	// id of this plugin
-        	'author'    => 'Warfare Plugins',	// author of this plugin
-        	'url'       => $website_url,
-            'beta'      => false // set to true if you wish customers to receive update notifications of beta releases
-            )
-        );
+            // setup the updater
+            $edd_updater = new SW_EDD_SL_Plugin_Updater( SWAWP_STORE_URL , __FILE__ , array(
+            	'version'   => SWAWP_VERSION,		// current version number
+            	'license'   => $license_key,	// license key
+            	'item_id'   => SWAWP_ITEM_ID,	// id of this plugin
+            	'author'    => 'Warfare Plugins',	// author of this plugin
+            	'url'       => $website_url,
+                'beta'      => false // set to true if you wish customers to receive update notifications of beta releases
+                )
+            );
+        }
     }
 }
 
@@ -82,16 +95,21 @@ function swawp_update_checker() {
  */
 function swawp_append_affiliate_id_to_links( $buttons ) {
 
-    // Check if the AffiliateWP plugin is installed
-    if ( function_exists('affwp_is_affiliate') ) {
+    // Make sure core is on a version that contains our dependancies
+    if (defined('SWP_VERSION') && version_compare(SWP_VERSION , SWAWP_CORE_VERSION_REQUIRED) >= 0){
 
-        // Check if the current user is logged in and is an affiliate
-        if ( ! ( is_user_logged_in() && affwp_is_affiliate() ) ) {
-            return $buttons;
+        // Check if the AffiliateWP plugin is installed
+        if ( function_exists('affwp_is_affiliate') ) {
+
+            // Check if the current user is logged in and is an affiliate
+            if ( ! ( is_user_logged_in() && affwp_is_affiliate() ) ) {
+                return $buttons;
+            }
+
+            // Append referral parameter and affiliate ID to sharing links in Social Warfare
+            $buttons['url'] = add_query_arg( affiliate_wp()->tracking->get_referral_var(), affwp_get_affiliate_id(), $buttons['url'] );
         }
 
-        // Append referral parameter and affiliate ID to sharing links in Social Warfare
-        $buttons['url'] = add_query_arg( affiliate_wp()->tracking->get_referral_var(), affwp_get_affiliate_id(), $buttons['url'] );
     }
 
     // Return the modified array
