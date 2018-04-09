61d62a00f3c305544c5371dc831bc8ef18ebc377
ckmahoney <cortland.mahoney@gmail.com>
2018-04-09 10:21:53 -0400

    Found old commit, this cleans up code.

--- social-warfare-affiliatewp.php
+++ social-warfare-affiliatewp.php
@@ -15,54 +15,43 @@ defined( 'WPINC' ) || die;
 add_action( 'plugins_loaded', function() {
 
     //* Define our new class here. Immediately instantiate below.
-    // class SWP_AffiliateWP extends SWP_Addon {
-    //     public function __construct( $name, $key, $product_id, $version, $core ) {
-    //         parent::__construct( $name, $key, $product_id, $version, $core );
-    //         add_filter( 'swp_network_buttons' , [$this, 'append_affiliate_id_to_links'], 1 , 1 );
-    //     }
-    //
-    //     /**
-    //      * Adds affiliate links to the share buttons.
-    //      *
-    //      * @since  1.0.0
-    //      * @access public
-    //      * @param  Array $buttons An array of information about the buttons being generated
-    //      * @return Array $buttons The modified array of information about the buttons being generated
-    //      *
-    //      */
-    //     public function append_affiliate_id_to_links( $buttons ) {
-    //
-    //         // Make sure core is on a version that contains our dependancies
-    //         if (defined('SWP_VERSION') && version_compare(SWP_VERSION , SWAWP_CORE_VERSION_REQUIRED) >= 0) {
-    //
-    //             // Check if the AffiliateWP plugin is installed
-    //             if ( function_exists('affwp_is_affiliate') ) {
-    //
-    //                 // Check if the current user is logged in and is an affiliate
-    //                 if ( ! ( is_user_logged_in() && affwp_is_affiliate() ) ) {
-    //                     return $buttons;
-    //                 }
-    //
-    //                 // Append referral parameter and affiliate ID to sharing links in Social Warfare
-    //                 $buttons['url'] = add_query_arg( affiliate_wp()->tracking->get_referral_var(), affwp_get_affiliate_id(), $buttons['url'] );
-    //             }
-    //         }
-    //
-    //         // Return the modified array
-    //         return $buttons;
-    //     }
-    // }
-    //
-    // $AffiliateWP = new SWP_AffiliateWP( 'Social Warfare - AffiliateWP', 'affiliatewp', 114264, '1.0.0', '2.3.2' );
-    //
+    class SWP_AffiliateWP extends SWP_Addon {
+        public function __construct( $name, $key, $product_id, $version, $core ) {
+            parent::__construct( $name, $key, $product_id, $version, $core );
+            add_filter( 'swp_network_buttons' , [$this, 'append_affiliate_id_to_links'], 1 , 1 );
+        }
 
-    class Test {
-        public function render() {
-            die("rendering");
+        /**
+         * Adds affiliate links to the share buttons.
+         *
+         * @since  1.0.0
+         * @access public
+         * @param  Array $buttons An array of information about the buttons being generated
+         * @return Array $buttons The modified array of information about the buttons being generated
+         *
+         */
+        public function append_affiliate_id_to_links( $buttons ) {
+
+            // Make sure core is on a version that contains our dependancies
+            if (defined('SWP_VERSION') && version_compare(SWP_VERSION , SWAWP_CORE_VERSION_REQUIRED) >= 0) {
+
+                // Check if the AffiliateWP plugin is installed
+                if ( function_exists('affwp_is_affiliate') ) {
+
+                    // Check if the current user is logged in and is an affiliate
+                    if ( ! ( is_user_logged_in() && affwp_is_affiliate() ) ) {
+                        return $buttons;
+                    }
+
+                    // Append referral parameter and affiliate ID to sharing links in Social Warfare
+                    $buttons['url'] = add_query_arg( affiliate_wp()->tracking->get_referral_var(), affwp_get_affiliate_id(), $buttons['url'] );
+                }
+            }
+
+            // Return the modified array
+            return $buttons;
         }
     }
 
-    $test = new Test();
-
-    $test->render();
+    $AffiliateWP = new SWP_AffiliateWP( 'Social Warfare - AffiliateWP', 'affiliatewp', 114264, '1.0.0', '2.3.2' );
 });
