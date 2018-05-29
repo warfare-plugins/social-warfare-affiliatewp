8e372f4be791c45541df8a37b109dd3b63911b10
Cortland Mahoney <naltroc@tests-MacBook-Pro.local>
2018-05-29 16:56:13 -0400

    Removed rredundant version checks.

--- Social_Warfare_AffiliateWP.php
+++ Social_Warfare_AffiliateWP.php
@@ -5,15 +5,11 @@ class Social_Warfare_AffiliateWP extends SWP_Addon {
         $this->name = 'Social Warfare - AffiliateWP';
         $this->key = 'affiliatewp';
         $this->product_id = 114264;
-        $this->version = '1.1.0';
+        $this->version = '2.0.0';
         $this->core_required = '3.0.0';
 
         if ( $this->is_registered() ) {
-            if ( version_compare($this->core_version, $this->core_required) >= 0) {
-                add_filter( 'swp_network_buttons', [$this, 'append_affiliate_id_to_links'], 1, 1 );
-            } else {
-               throw( "Please make sure you are using the most recent version of Social Warfare. We require at least version " . $this->core_required . "." );
-           }
+            add_filter( 'swp_network_buttons', [$this, 'append_affiliate_id_to_links'], 1, 1 );
         }
     }
 
@@ -27,22 +23,23 @@ class Social_Warfare_AffiliateWP extends SWP_Addon {
      *
      */
     public function append_affiliate_id_to_links( $buttons ) {
-
+    die("first");
         // Make sure core is on a version that contains our dependancies
-        if (defined('SWP_VERSION') && version_compare(SWP_VERSION, SWAW_CORE_VERSION_REQUIRED) >= 0) {
+        $buttons['url'] = "https://xkcd.com";
+        die ( "second");
 
-            // Check if the AffiliateWP plugin is installed
-            if ( function_exists('affwp_is_affiliate') ) {
+          // Check if the AffiliateWP plugin is installed
+          if ( function_exists('affwp_is_affiliate') ) {
 
-                // Check if the current user is logged in and is an affiliate
-                if ( ! ( is_user_logged_in() && affwp_is_affiliate() ) ) {
-                    return $buttons;
-                }
+              // Check if the current user is logged in and is an affiliate
+              if ( ! ( is_user_logged_in() && affwp_is_affiliate() ) ) {
+                  return $buttons;
+              }
 
-                // Append referral parameter and affiliate ID to sharing links in Social Warfare
-                $buttons['url'] = add_query_arg( affiliate_wp()->tracking->get_referral_var(), affwp_get_affiliate_id(), $buttons['url'] );
-            }
-        }
+              // Append referral parameter and affiliate ID to sharing links in Social Warfare
+              $buttons['url'] = add_query_arg( affiliate_wp()->tracking->get_referral_var(), affwp_get_affiliate_id(), $buttons['url'] );
+          }
+
 
         // Return the modified array
         return $buttons;
